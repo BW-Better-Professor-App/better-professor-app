3591e47ddff5398dd3340e3e4d3eb0d41fd67174
Westley Feller <feller.westley@gmail.com>
2019-10-16 20:00:04 -0700

    SingleStudentPage structure built

---
+++ professor-front-end/src/components/SingleStudentPage.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Card, CardBody,
+  CardHeader,
+  CardTitle,
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading, ListGroupItemText
+} from 'reactstrap';
+
+
+const SingleStudentPage = ({ student }) => {
+  return (
+    <>
+      <h1>{`${student.firstname} ${student.lastname}`}</h1>
+      <p>{student.email}</p>
+
+      <h2>Projects</h2>
+      <ListGroup>
+        {student.projects.map((project) => (
+          <Card key={project.id}>
+            <CardHeader>
+              <CardTitle>{project.project_name}</CardTitle>
+            </CardHeader>
+
+            <CardBody>
+              <ListGroup flush>
+                <ListGroupItem>
+                  <ListGroupItemHeading>Project Deadline</ListGroupItemHeading>
+                  <ListGroupItemText>{project.project_deadline}</ListGroupItemText>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <ListGroupItemHeading>Feedback Deadline</ListGroupItemHeading>
+                  <ListGroupItemText>{project.feedback_deadline}</ListGroupItemText>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <ListGroupItemHeading>Recommendation Deadline</ListGroupItemHeading>
+                  <ListGroupItemText>{project.recommendation_deadline}</ListGroupItemText>
+                </ListGroupItem>
+
+                <ListGroupItem>
+                  <ListGroupItemHeading>Student Message</ListGroupItemHeading>
+                  <ListGroupItemText>{project.studentMessage}</ListGroupItemText>
+                </ListGroupItem>
+
+                <ListGroupItem>
+                  <ListGroupItemHeading>Professor Message</ListGroupItemHeading>
+                  <ListGroupItemText>{project.professorMessage}</ListGroupItemText>
+                </ListGroupItem>
+              </ListGroup>
+            </CardBody>
+          </Card>
+        ))}
+      </ListGroup>
+    </>
+  )
+};
+
+export default SingleStudentPage;
