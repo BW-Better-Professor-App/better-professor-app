f242a8bd077f0ecfabca648b71b7fc0f211b1ef4
Logan Karnes <logan.karnes98@gmail.com>
2019-10-16 19:24:00 -0700

    routes added make sure to switch to private route later one

--- professor-front-end/src/App.js
+++ professor-front-end/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './components/HomePage';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
+import StudentList from './components/StudentList';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path='/' component={HomePage}/>
-
+        <Route path='/login' component={LogIn}/>
+        <Route path='/signup' component={SignUp}/>
+        <Route path='/studentlist' component={StudentList}/>
       </div>
     </Router>
     
