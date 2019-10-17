1b1ae5bef76e448aeed690530490e977fd17c868
CodingCCarpenter <christineassists@aol.com>
2019-10-16 23:08:45 -0500

    make sure add student is cleared

--- professor-front-end/src/App.js
+++ professor-front-end/src/App.js
@@ -15,9 +15,7 @@ function App() {
         <Route path='/login' component={LogIn}/>
         <Route path='/signup' component={SignUp}/>
         <Route path='/studentlist' component={StudentList}/>
-        <Route path='/login' component={LogIn}/>
-        <Route path='/signup' component={SignUp}/>
-        <Route path='/studentlist' component={StudentList}/>
+       
 
       </div>
     </Router>
