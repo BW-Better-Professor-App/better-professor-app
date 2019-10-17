57194c6f54b8202dbbf3c2bacaa3264f4c35e6b7
Westley Feller <feller.westley@gmail.com>
2019-10-16 20:00:30 -0700

    Added bootstrap for reactstrap styling

--- professor-front-end/src/App.js
+++ professor-front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/HomePage';
 import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
@@ -16,7 +17,7 @@ function App() {
         <Route path='/studentlist' component={StudentList}/>
       </div>
     </Router>
-    
+
   );
 }
 
